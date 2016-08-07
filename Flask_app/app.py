5d685e7f1edd57e6bad82cffcbc85c8eda6af73f
shihyinnn <schen102@dons.usfca.edu>
2016-08-07 23:55:08 +0800

    Fix the plot spectrum function and add a form to it. Also clean up the commented codes.

--- Flask_app/app.py
+++ Flask_app/app.py
@@ -1,9 +1,11 @@
 import os
 import sys
-from flask import Flask, render_template, request, redirect, url_for, send_from_directory, jsonify
+from flask import Flask, render_template, request, redirect, url_for, \
+    send_from_directory, jsonify
 from werkzeug import secure_filename
 import pymzml
-from wtforms import Form, IntegerField, validators, StringField, DecimalField
+from wtforms import Form, IntegerField, validators, StringField, DecimalField, \
+    FloatField
 
 # Initialize the Flask application
 app = Flask(__name__)
@@ -81,32 +83,45 @@ def peak_finding(filename):
                 peak_to_find = spectrum.hasPeak(mz_value)
                 if peak_to_find:
                     lst.append(peak_to_find)
-        return render_template('peak_to_find.html', form=form, filename=filename, lst=lst)
+        return render_template('peak_to_find.html', form=form,
+                               filename=filename, lst=lst)
 
     # Create the form (the first time the page loads)
     return render_template('peak_to_find.html', form=form, filename=filename)
 
 
+class PlotSpectrumForm(Form):
+    # mz_range_start = DecimalField('Start')
+    # mz_range_end = DecimalField('End')
+    mz_range_start = FloatField('Start')
+    mz_range_end = FloatField('End')
 
 
-@app.route('/plot_spectrum/<filename>')
+@app.route('/plot_spectrum/<filename>', methods=["GET", "POST"])
 def plot_spectrum(filename):
     filepath = get_abs_path() + "/uploads/" + filename
     run = pymzml.run.Reader(filepath, MSn_Precision=25e-6)
-    p = pymzml.plot.Factory()
-    for spec in run:
-        p.newPlot()
-        p.add(spec.peaks, color=(200, 00, 00), style='circles')
-        p.add(spec.centroidedPeaks, color=(00, 00, 00), style='sticks')
-        p.add(spec.reprofiledPeaks, color=(00, 255, 00), style='circles')
-        p.save(filename="static/tmp/plotAspect.xhtml")
-        # p.save(filename="templates/plotAspect.xhtml")
-    # return render_template('plotAspect.xhtml')
 
-    return render_template('plotAspect.html',
-                           fig=url_for('static',
-                                       filename='tmp/plotAspect.xhtml'))
+    form = PlotSpectrumForm(request.form)
 
+    if request.method == 'POST' and form.validate():
+
+        start = form.mz_range_start.data
+        end = form.mz_range_end.data
+
+        p = pymzml.plot.Factory()
+        for spec in run:
+            p.newPlot()
+            p.add(spec.peaks, color=(200, 00, 00), style='circles')
+            p.add(spec.centroidedPeaks, color=(00, 00, 00), style='sticks')
+            p.add(spec.reprofiledPeaks, color=(00, 255, 00), style='circles')
+            p.save(filename="static/tmp/plotAspect.xhtml", mzRange=[start, end])
+        return render_template('plotAspect.html',
+                               fig=url_for('static',
+                                           filename='tmp/plotAspect.xhtml'),
+                               form=form, p=p)
+
+    return render_template('plotAspect.html', form=form, filename=filename)
 
 
 class HighestPeakForm(Form):
@@ -136,19 +151,15 @@ def highest_peaks(filename):
                 if spectrum["id"] == id_value:
                     for mz, i in spectrum.highestPeaks(num_of_peaks):
                         lst.append((mz, i))
+
         return render_template('highest_peaks.html', form=form,
                                filename=filename, lst=lst)
-            # else:
-            #     return "Peak does not exist."
     return render_template('highest_peaks.html', form=form, filename=filename)
-
 
 
 class IonChromatogramForm(Form):
     mz2find = DecimalField('mz value which should be found')
     spectrum_ms_level = IntegerField('Spectrum MS Level')
-
-
 
 
 @app.route('/extract_Ion_Chromatogram/<filename>', methods=["GET", "POST"])
@@ -161,10 +172,9 @@ def extract_Ion_Chromatogram(filename):
 
     if request.method == 'POST' and form.validate():
 
-        mz2find = form.mz2find.data
-        spectrum_ms_level = form.spectrum_ms_level.data
+        mz2find = form.mz2find.data  # 810.53
+        spectrum_ms_level = form.spectrum_ms_level.data  # 1
 
-    # MASS_2_FOLLOW = 810.53
         for spectrum in run:
             if spectrum['ms level'] == spectrum_ms_level:
                 matchList = spectrum.hasPeak(mz2find)
@@ -180,35 +190,11 @@ def extract_Ion_Chromatogram(filename):
 
         json_dict = jsonify(**diction)
         return render_template('ion_chromatogram.html', form=form,
-                               filename=filename, json_dict=json_dict, diction=diction)
+                               filename=filename, json_dict=json_dict,
+                               diction=diction)
 
-    return render_template('ion_chromatogram.html', form=form, filename=filename)
-
-
-
-# @app.route('/extract_Ion_Chromatogram/<filename>', methods=["GET", "POST"])
-# def extract_Ion_Chromatogram(filename):
-#     filepath = get_abs_path() + "/uploads/" + filename
-#     run = pymzml.run.Reader(filepath, MS1_Precision=20e-6, MSn_Precision=20e-6)
-#     timeDependentIntensities = []
-#
-#     MASS_2_FOLLOW = 810.53
-#     for spectrum in run:
-#         if spectrum['ms level'] == 1:
-#             matchList = spectrum.hasPeak(MASS_2_FOLLOW)
-#             if matchList != []:
-#                 for mz, I in matchList:
-#                     timeDependentIntensities.append(
-#                         [spectrum['scan start time'], I, mz])
-#     diction = {'rt': [], 'i': [], 'mz': []}
-#     for rt, i, mz in timeDependentIntensities:
-#         diction['rt'].append(round(rt, 3))
-#         diction['i'].append(round(i, 4))
-#         diction['mz'].append(round(mz, 10))
-#
-#     json_dict = jsonify(diction)
-#     return json_dict
-
+    return render_template('ion_chromatogram.html', form=form,
+                           filename=filename)
 
 
 @app.route('/original_XML_Tree/<filename>')
@@ -219,5 +205,7 @@ def original_XML_Tree(filename):
     return render_template('original_XML_Tree.html',
                            spectrum_xmlTree=spectrum.xmlTree)
 
+
 if __name__ == '__main__':
-    app.run(port=4555, debug=True)
+    # app.run(port=4555, debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
